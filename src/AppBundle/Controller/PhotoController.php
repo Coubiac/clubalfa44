f506c8df45a44852410b5532cec277c5cd28c2cc
Coubiac <benoit.grisot@gmail.com>
2017-11-09 22:22:20 +0100

    ajout controller photo

---
+++ src/AppBundle/Controller/PhotoController.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ben
+ * Date: 08/11/2017
+ * Time: 14:53
+ */
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class PhotoController extends Controller
+{
+
+    /**
+     * @Method("GET")
+     * @Route("/photos", name="photos")
+     */
+    public function photosAction()
+    {
+        $photos = $this
+            ->getDoctrine()
+            ->getManager()
+            ->getRepository('AppBundle:Photo')
+            ->findAll();
+
+        return $this->render('photos/photos.html.twig', array(
+            'photos' => $photos,
+        ));
+    }
+
+}
