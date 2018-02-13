0c79cd59b458b27b8097f274caab7801bf1cb1ee
Coubiac <benoit.grisot@gmail.com>
2018-02-13 14:41:17 +0100

    reduction du menu et correction logo

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,26 +9,26 @@ $(document).ready(function(){
         $('.preloader-background').remove();
     }
     if (screenWidth > 800){
-        $("#video").html('<video class="hide-on-med-and-down" id="video1" autoplay loop style="width: 100%"><source src="videos/alfa.webm" type="video/webm" /><source src="videos/alfa.mp4" type="video/mp4"><source src="videos/alfa.ogv" type="video/ogv" /><p>Your browser does not support the video tag.</p></video>');
-
-
+        $("#video").html('<video class="hide-on-med-and-down" id="video1" autoplay loop style="width: 100%"><source src="videos/alfa.mp4" type="video/mp4" /><source src="videos/alfa.webm" type="video/webm"><source src="videos/alfa.ogv" type="video/ogg" /><p>Your browser does not support the video tag.</p></video>');
         $('.preloader-background').delay(1700).fadeOut('slow');
-
         $('.preloader-wrapper')
             .delay(1700)
-            .fadeOut();
-
-    }
+            .fadeOut();}
     $('#video1').get(0).play();
 
 
 
-    $('.button-collapse').sideNav({'edge': 'left'});
+    $('.button-collapse').sideNav({
+        'menuWidth': 240,
+        'edge': 'left'
+    });
     $('select').material_select();
 
     $('.materialboxed').materialbox();
 
     var count = $('.count-chars');
+
+
 //script pour compter les caractères
     $('.textarea-contact').keyup(function () {
 
