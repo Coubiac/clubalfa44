431b4f2109e793cc475ceefcfc7847e7d5934984
Coubiac <benoit.grisot@gmail.com>
2018-04-13 18:54:35 +0200

    ajout des tarifs

---
+++ src/AppBundle/Controller/TarifsController.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ben
+ * Date: 08/11/2017
+ * Time: 14:53
+ */
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class TarifsController extends Controller
+{
+
+    /**
+     * @Method("GET")
+     * @Route("/tarifs", name="tarifs")
+     */
+    public function tarifsAction()
+    {
+        return $this->render('tarifs/tarifs.html.twig');
+    }
+
+}
