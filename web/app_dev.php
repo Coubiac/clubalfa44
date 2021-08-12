a7093bc9dde3b9fd94be81a89d82af30df378369
Benoit <benoit.grisot@gmail.com>
2021-08-12 08:57:25 +0200

    backup before archiving

--- web/app_dev.php
+++ web/app_dev.php
@@ -13,7 +13,7 @@ use Symfony\Component\HttpFoundation\Request;
 
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1', '109.31.136.195', '37.58.173.253'], true) || PHP_SAPI === 'cli-server')
+    || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1', '109.31.136.195', '37.58.173.253', '37.171.163.178'], true) || PHP_SAPI === 'cli-server')
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
